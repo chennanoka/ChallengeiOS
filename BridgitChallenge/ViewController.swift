24766a9f0008b2e4a289723d81d8bcf81a8973ef
Nan Chen <nan@gobridgit.com>
2017-11-10 15:36:21 -0500

    Initial Commit

---
+++ BridgitChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BridgitChallenge
+//
+//  Created by Nan Chen on 2017-11-10.
+//  Copyright © 2017 Nan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
